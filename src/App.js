c628a9754ea6312ea72c17bc71a74df01f32f824
2Nadim <nadimmahamud661@gmail.com>
2022-10-26 02:09:32 +0100

    title added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className='text-primary'>start assignment</h1>
+      <h1 className='text-primary'>start assignment!!</h1>
     </div>
   );
 }
