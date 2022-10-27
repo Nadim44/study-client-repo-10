3dc6fcca34acab0588ec8ab6ad45e0942b8e219e
2Nadim <nadimmahamud661@gmail.com>
2022-10-27 17:55:31 +0100

    something change and final commit

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -7,36 +7,38 @@ const FAQ = () => {
         <div>
             <Header></Header>
             <Container>
-                <div className='bg-success p-2 rounded mb-4'>
-                    <h1>Some Important Question!!!</h1>
-                    <h3>
-                        Q:- What is Firebase?
-                    </h3>
-                    <p>Answer: <span>Firebase provides tools to grow your app and business, for startups & global enterprises. Get your app up and running quickly & securely with fully managed backend infrastructure. </span></p>
-                </div>
-                <div className='bg-success p-2 rounded mb-4'>
-                    <h3>
-                        Q:- What is firebase architecture?
-                    </h3>
-                    <p>Answer: <span>In this architecture, Firebase sits between the server and clients. Your servers can connect to Firebase and interact with the data just like any other client would. In other words, your server communicates with clients by manipulating data in Firebase.</span></p>
-                </div>
-                <div className='bg-success p-2 rounded mb-4'>
-                    <h3>
-                        Q:- What are the features of firebase?
-                    </h3>
-                    <p>Answer: <span>Realtime Database. Build serverless apps by storing and syncing JSON data between your users in near-realtime, on or offline, with strong user-based security. </span></p>
-                </div>
-                <div className='bg-success p-2 rounded mb-4'>
-                    <h3>
-                        Q:- Can you tell me names of 3 authentication method?
-                    </h3>
-                    <p>Answer: <span> 1. Google 2.Github 3.Email & Password</span></p>
-                </div>
-                <div className='bg-success p-2 rounded mb-4'>
-                    <h3>
-                        Q:- What is Firebase?
-                    </h3>
-                    <p>Answer: <span></span></p>
+                <h1 className='m-2'>Some Important Question!!!</h1>
+                <div>
+                    <div className='bg-success p-2 rounded mb-4'>
+                        <h3>
+                            Q:- What is Firebase?
+                        </h3>
+                        <p>Answer: <span>Firebase provides tools to grow your app and business, for startups & global enterprises. Get your app up and running quickly & securely with fully managed backend infrastructure. </span></p>
+                    </div>
+                    <div className='bg-success p-2 rounded mb-4'>
+                        <h3>
+                            Q:- What is firebase architecture?
+                        </h3>
+                        <p>Answer: <span>In this architecture, Firebase sits between the server and clients. Your servers can connect to Firebase and interact with the data just like any other client would. In other words, your server communicates with clients by manipulating data in Firebase.</span></p>
+                    </div>
+                    <div className='bg-success p-2 rounded mb-4'>
+                        <h3>
+                            Q:- What are the features of firebase?
+                        </h3>
+                        <p>Answer: <span>Realtime Database. Build serverless apps by storing and syncing JSON data between your users in near-realtime, on or offline, with strong user-based security. </span></p>
+                    </div>
+                    <div className='bg-success p-2 rounded mb-4'>
+                        <h3>
+                            Q:- Can you tell me names of 3 authentication method?
+                        </h3>
+                        <p>Answer: <span> 1. Google 2.Github 3.Email & Password</span></p>
+                    </div>
+                    <div className='bg-success p-2 rounded mb-4'>
+                        <h3>
+                            Q:- What is Firebase?
+                        </h3>
+                        <p>Answer: <span></span></p>
+                    </div>
                 </div>
             </Container>
         </div>
